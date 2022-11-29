2f18ab428683667c0cee9851c907c503398e150d
Mehal <78313436+MEHALSAKTHI@users.noreply.github.com>
2022-11-29 16:20:39 +0530

    Removed sgd

--- models/CGE.py
+++ models/CGE.py
@@ -48,10 +48,10 @@ class CGEConv:
         model = tf.keras.Model(inputs=[input1, input2], outputs=[prediction])
 
         model.summary()
-        adama = tf.keras.optimizers.Adam(0.0005)
-        from tensorflow.keras.optimizers import Adam,Nadam, SGD
-        optimizer = SGD(lr=0.1, decay=1e-6, momentum=0, nesterov=False)
-        model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+#         adama = tf.keras.optimizers.Adam(0.0005)
+#         from tensorflow.keras.optimizers import Adam,Nadam, SGD
+#         optimizer = SGD(lr=0.1, decay=1e-6, momentum=0, nesterov=False)
+        model.compile(optimizer=adama, loss='binary_crossentropy', metrics=['accuracy'])
         self.model = model
 
     """
