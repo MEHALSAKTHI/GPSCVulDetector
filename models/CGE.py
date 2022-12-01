4a7b7fdcda1e4bc8223f5c3fae16b428e5c69c44
Mehal <78313436+MEHALSAKTHI@users.noreply.github.com>
2022-12-01 09:38:43 +0530

    Update CGE.py

--- models/CGE.py
+++ models/CGE.py
@@ -63,21 +63,13 @@ class CGEConv:
     """
 
     def train(self):
-        n_folds = 10
-        cv_scores, model_history = list(), list()
-        for _ in range(n_folds):
-            # split data
+
             X_train1, X_val1, y_train, y_val = train_test_split(self.graph_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
             X_train2, X_val2, y_train, y_val = train_test_split(self.pattern_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
-            # evaluate model
-            model, test_acc = evaluate_model([X_train1,X_train2], [X_val1,X_Val2], y_train, y_val)
-            print('>%.3f' % val_acc)
-            cv_scores.append(val_acc)
-            model_history.append(model)
     
         print('Estimated Accuracy %.3f (%.3f)' % (np.mean(cv_scores), np.std(cv_scores)))
     
-        self.model.fit([self.graph_train, self.pattern_train], self.y_train, batch_size=self.batch_size,
+        self.model.fit([self.graph_train, self.pattern_train], self.y_train, validation_data= [X_val1, X_val2 ], batch_size=self.batch_size,
                        epochs=100)
                        #class_weight=self.class_weight)
         # self.model.save_weights("model.pkl")
