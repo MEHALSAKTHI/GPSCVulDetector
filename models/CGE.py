6fd5f8cec79c1739497ea4f28ec00606e497c862
Mehal <78313436+MEHALSAKTHI@users.noreply.github.com>
2022-12-01 09:55:29 +0530

    Update CGE.py

--- models/CGE.py
+++ models/CGE.py
@@ -64,15 +64,15 @@ class CGEConv:
 
     def train(self):
 
-        X_train1, X_val1, y_train, y_val = train_test_split([self.graph_train,self.pattern_train], self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
-#         X_train2, X_val2, y_train, y_val = train_test_split(self.pattern_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
+        X_train1, X_val1, y_train, y_val = train_test_split(self.graph_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
+        X_train2, X_val2, y_train, y_val = train_test_split(self.pattern_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
     
 #         print('Estimated Accuracy %.3f (%.3f)' % (np.mean(cv_scores), np.std(cv_scores)))
         print("Train: ")
         print([self.graph_train, self.pattern_train])
         print("Val: ")
-        print(X_val1)
-        self.model.fit([self.graph_train, self.pattern_train], self.y_train, validation_data= X_val1, batch_size=self.batch_size,
+        print([X_val1,X_val2])
+        self.model.fit([self.graph_train, self.pattern_train], self.y_train, validation_data= [X_val1,X_val2], batch_size=self.batch_size,
                        epochs=100)
                        #class_weight=self.class_weight)
         # self.model.save_weights("model.pkl")
