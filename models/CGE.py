4fe6b083d6cf6adea169c20667ab51cf519a0648
Mehal <78313436+MEHALSAKTHI@users.noreply.github.com>
2023-01-27 01:04:11 +0530

    Update CGE.py

--- models/CGE.py
+++ models/CGE.py
@@ -89,9 +89,13 @@ class CGEConv:
     def train(self):
 
         X_train1, X_val1, y_train, y_val = train_test_split(self.graph_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
-        X_train2, X_val2, y_train, y_val = train_test_split(self.pattern_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
+#         X_train2, X_val2, y_train, y_val = train_test_split(self.pattern_train, self.y_train, test_size=0.10, random_state = np.random.randint(1,1000, 1)[0])
         
         print("Log Reg")
+        print("X_train1")
+        print(X_train1)
+        print("y_train")
+        print(y_train)
         
         logreg = LogisticRegression()
         logreg.fit(X_train1, y_train)
